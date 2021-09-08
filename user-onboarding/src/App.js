dcbf15a5d768e080b0db7c089312557653de15cc
Daniel <dek.kneale@gmail.com>
2021-09-07 22:28:23 -0700

    mvp

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Form from './component/Form'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>User Onboarding</h1>
+      <Form />  
+    </div>
+  );
+}
+
+export default App;
